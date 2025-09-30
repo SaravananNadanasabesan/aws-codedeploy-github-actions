32a45af2d1d5cdd4ed35f09c2b30fa5b9a44a298
8c5BO2VvipedhY <saravanasabesan94@gmail.com>
2025-09-30 17:10:25 -0400

    final update

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -xe
 
+# Define variables
+WAR_NAME=SpringBootHelloWorldExampleApplication.war
+S3_BUCKET=codedeploystacklab3-saravan-webappdeploymentbucket-u8nnusmih6oi
+TOMCAT_WEBAPPS_DIR=/opt/tomcat/webapps
 
-# Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeploystacklab3-saravan-webappdeploymentbucket-u8nnusmih6oi/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+# Download the WAR file from S3
+echo "Downloading $WAR_NAME from S3 bucket: $S3_BUCKET"
+aws s3 cp s3://$S3_BUCKET/$WAR_NAME $TOMCAT_WEBAPPS_DIR/$WAR_NAME
 
-
-# Ensure the ownership permissions are correct.
-chown -R tomcat:tomcat /usr/local/tomcat9/webapps
+# Set permissions (optional if Tomcat runs as ec2-user)
+chown -R tomcat:tomcat $TOMCAT_WEBAPPS_DIR
